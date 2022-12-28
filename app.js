0715280ce5625badd6354188724cf757eff3ca50
Jyotikumari99 <chunniyadav99@gmail.com>
2022-12-28 20:29:31 +0530

    finally responsive

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ function changeCircleColor(color)
   const circle=document.querySelector('.circle');
   circle.style.background = color;
 }
+function menutoggle(){
+     const toggleMenu=document.querySelector('.toggleMenu');
+     const navigation=document.querySelector('.navigation');
+     toggleMenu.classList.toggle('active');
+     navigation.classList.toggle('active');
+}
